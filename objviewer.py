48af6be96456678d203b644fdcc710504aa232f3
wuhk76 <wuhk76@gmail.com>
2025-08-02 13:01:20 -0700

    Add files via upload

---
+++ objviewer.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+def parseobj(objpath):
+    points = []
+    with open(objpath) as obj:
+        obj = obj.readlines()
+    for line in obj:
+        line = line.strip()
+        if line.startswith('v '):
+            line = line.split()
+            del line[0]
+            line = [float(value.strip()) for value in line]
+            point = tuple(line)
+            points.append(point)
+    return points
+mesh = parseobj(input('Enter path of OBJ:'))
+x, y, z = zip(*mesh)
+fig = plt.figure()
+ax = fig.add_subplot(111, projection = '3d')
+ax.scatter(x, y, z, s = 1, color = 'blue')
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_zlabel('z')
+plt.show(block=True)
